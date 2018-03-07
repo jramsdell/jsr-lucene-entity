d52624f899b1c5bf23fbdf3ebe0dc69d860a0a07
jramsdell <ramsdell.jordan@gmail.com>
2018-03-07 15:52:27 -0500

    Add parsing

--- src/main/kotlin/edu/unh/cs980/KotlinCborParser.kt
+++ src/main/kotlin/edu/unh/cs980/KotlinCborParser.kt
@@ -14,9 +14,19 @@ fun getStuff(filename: String, databaseName: String) {
     val kotIndexer = HyperlinkIndexer(databaseName)
     val f = File(filename).inputStream().buffered(16 * 1024)
     val clean = {string: String -> string.toLowerCase().replace(" ", "_")}
+    val counter = AtomicInteger()
 
     DeserializeData.iterableAnnotations(f)
         .forEachParallel { page ->
+
+            // This is just to keep track of how many pages we've parsed
+            counter.incrementAndGet().let {
+                if (it % 100000 == 0) {
+                    println(it)
+                }
+            }
+
+            // Extract all of the anchors/entities and add them to database
             page.flatSectionPathsParagraphs()
                 .flatMap { psection ->
                     psection.
